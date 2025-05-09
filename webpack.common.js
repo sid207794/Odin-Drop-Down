8675952363758541670682e1ffe00173477b0760
sid207794 <193688659+sid207794@users.noreply.github.com>
2025-05-09 17:03:04 +0530

    Initial commit

---
+++ webpack.common.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+const flatpickr = require("flatpickr");// require("flatpickr/dist/themes/material_blue.css");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        filename: "main.bundle.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: "./src/template.html",
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+        ],
+    },
+};
